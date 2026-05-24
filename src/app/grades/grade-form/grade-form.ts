729e40bf5a074d758a33fd6b65109c427aaa1e36
Camilo9237 <german.hernandez02@uptc.edu.co>
2026-05-24 16:32:49 -0500

    modulo grades comienzo

---
+++ src/app/grades/grade-form/grade-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grade-form',
+  imports: [],
+  templateUrl: './grade-form.html',
+  styleUrl: './grade-form.css'
+})
+export class GradeForm {
+
+}
